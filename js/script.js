08126bc4706ed7b93f4add82d2132734837766da
ramilbacay <ramilbacay@gmail.com>
2018-04-30 14:45:11 +0800

    index, js
    Added autofocus and send chat using button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.getElementById('chatArea').onkeyup = function(event){
     
-
     if (event.keyCode == '13'){
     var text = document.getElementById("chatArea").value;
     var message = document.createElement("div");
@@ -15,12 +14,17 @@ document.getElementById('chatArea').onkeyup = function(event){
     
     }
   }
-  // document.getElementById('send').onclick = function(e){
+
+  document.getElementById('sendBtn').onclick = function(event){
     
-  //   var text = document.getElementById("add_child").value;
-  //   var div = document.getElementById('child');
-  //   var childHtml = "<div class='child'>" + text + "</div>"; 
-  //   div.innerHTML += childHtml;
-  //   var message = document.getElementById('add_child');
-  //   document.getElementById("add_child").value = '';
-  // }
+    var text = document.getElementById("chatArea").value;
+    var message = document.createElement("div");
+    var node = document.createTextNode(text);
+    message.appendChild(node);
+    message.className = "chat"
+	var element = document.getElementById("chatDisplay");
+	element.appendChild(message);
+    document.getElementById("chatArea").value = '';
+    var scrollDown = document.getElementById('chatDisplay');
+  	scrollDown.scrollTop = scrollDown.scrollHeight;
+  }
