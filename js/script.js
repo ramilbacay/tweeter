b32f42099b9bcef7b347a90636e1f2fd64951774
ramilbacay <ramilbacay@gmail.com>
2018-04-30 14:26:30 +0800

    index, style, js
    Added style, chat box, and javascript.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+document.getElementById('chatArea').onkeyup = function(event){
+    
 
+    if (event.keyCode == '13'){
+    var text = document.getElementById("chatArea").value;
+    var message = document.createElement("div");
+    var node = document.createTextNode(text);
+    message.appendChild(node);
+    message.className = "chat"
+	var element = document.getElementById("chatDisplay");
+	element.appendChild(message);
+    document.getElementById("chatArea").value = '';
+    var scrollDown = document.getElementById('chatDisplay');
+  	scrollDown.scrollTop = scrollDown.scrollHeight;
+    
+    }
+  }
+  // document.getElementById('send').onclick = function(e){
+    
+  //   var text = document.getElementById("add_child").value;
+  //   var div = document.getElementById('child');
+  //   var childHtml = "<div class='child'>" + text + "</div>"; 
+  //   div.innerHTML += childHtml;
+  //   var message = document.getElementById('add_child');
+  //   document.getElementById("add_child").value = '';
+  // }
