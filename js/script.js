d7072bf9b815ac1085017aef387b0deac5b78d20
ramilbacay <ramilbacay@gmail.com>
2018-04-30 15:36:18 +0800

    js
    Added validation for all whitespace input

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
 document.getElementById('chatArea').onkeyup = function(event){
     
     if (event.keyCode == '13'){
-    var text = document.getElementById("chatArea").value;
-    var message = document.createElement("div");
-    var node = document.createTextNode(text);
-    message.appendChild(node);
-    message.className = "chat"
-	var element = document.getElementById("chatDisplay");
-	element.appendChild(message);
-    document.getElementById("chatArea").value = '';
-    var scrollDown = document.getElementById('chatDisplay');
-  	scrollDown.scrollTop = scrollDown.scrollHeight;
-    
+    	var text = document.getElementById("chatArea").value;
+    	if (text.trim() === "") {
+    		document.getElementById("chatArea").value = '';
+    	}
+
+    	else {
+		    var message = document.createElement("div");
+		    var node = document.createTextNode(text);
+		    message.appendChild(node);
+		    message.className = "chat"
+			var element = document.getElementById("chatDisplay");
+			element.appendChild(message);
+		    document.getElementById("chatArea").value = '';
+		    var scrollDown = document.getElementById('chatDisplay');
+		  	scrollDown.scrollTop = scrollDown.scrollHeight;
+    	}
     }
   }
 
   document.getElementById('sendBtn').onclick = function(event){
     
     var text = document.getElementById("chatArea").value;
-    var message = document.createElement("div");
-    var node = document.createTextNode(text);
-    message.appendChild(node);
-    message.className = "chat"
-	var element = document.getElementById("chatDisplay");
-	element.appendChild(message);
-    document.getElementById("chatArea").value = '';
-    var scrollDown = document.getElementById('chatDisplay');
-  	scrollDown.scrollTop = scrollDown.scrollHeight;
+    if (text.trim() === "") {
+    		document.getElementById("chatArea").value = '';
+    	}
+
+    	else {
+		    var message = document.createElement("div");
+		    var node = document.createTextNode(text);
+		    message.appendChild(node);
+		    message.className = "chat"
+			var element = document.getElementById("chatDisplay");
+			element.appendChild(message);
+		    document.getElementById("chatArea").value = '';
+		    var scrollDown = document.getElementById('chatDisplay');
+		  	scrollDown.scrollTop = scrollDown.scrollHeight;
+  		}
   }
